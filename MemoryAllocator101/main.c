5e2421aa034ff345b3ddfa1cc221602dd07446ce
RoshanS21 <roshan.shrestha0621@gmail.com>
2024-03-29 15:01:17 -0500

    * add head and tail pointers to keep track of the list * add lock to prevent deadlock * modify malloc

--- MemoryAllocator101/main.c
+++ MemoryAllocator101/main.c
@@ -1,17 +1,57 @@
 #include <stdio.h>
 #include <memory.h>
+#include <pthread.h>
 
 typedef char ALIGN[16];
+header_t *head, *tail;
+pthread_mutex_t global_malloc_lock;
+
+header_t *get_free_block(size_t size)
+{
+    header_t *curr = head;
+    while(curr)
+    {
+        if(curr->s.is_free && curr->s.size >= size)
+            return curr;
+        curr = curr->s.next;
+    }
+    return NULL;
+}
 
 void *malloc(size_t size)
 {
+    size_t total_size;
     void *block;
-    block = sbrk(size);
+    header_t *header;
+    if(!size)
+        return NULL;
+    pthread_mutex_lock(&global_malloc_lock);
+    header = get_free_block(size);
+    if(header)
+    {
+        header->s.is_free = 0;
+        pthread_mutex_unlock(&global_malloc_lock);
+        return (void*)(header + 1);
+    }
+    total_size = sizeof(header_t) + size;
+    block = sbrk(total_size);
     if (block == (void*) -1)
     {
+        pthread_mutex_unlock(&global_malloc_lock);
         return NULL;
     }
-    return block;
+    header = block;
+    header->s.size = size;
+    header->s.is_free = 0;
+    header->s.next = NULL;
+    if(!head)
+        head = header;
+    if(tail)
+        tail->s.next = header;
+    tail = header;
+    pthread_mutex_unlock(&global_malloc_lock);
+
+    return (void*)(header + 1);
 }
 
 union header
@@ -25,4 +65,3 @@ union header
     ALIGN stub;
 };
 typedef union header header_t;
-

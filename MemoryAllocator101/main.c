a4231febf651198e89953e524265eba19c5da08a
RoshanS21 <roshan.shrestha0621@gmail.com>
2024-03-29 14:13:08 -0500

    ground work

---
+++ MemoryAllocator101/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <memory.h>
+
+void *malloc(size_t size)
+{
+    void *block;
+    block = sbrk(size);
+    if (block == (void*) -1)
+    {
+        return NULL;
+    }
+    return block;
+}
+
+struct header_t
+{
+    size_t size;
+    unsigned is_free;
+};
+
